bd2e2d946ab9caf45da7d4d01143364b3d25e921
manthan <manthan.jsharma@gmail.com>
2025-03-16 19:48:20 +0530

    roll up config

---
+++ src/utils/rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+  },
+];
